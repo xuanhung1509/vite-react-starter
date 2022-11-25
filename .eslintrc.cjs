861b9ae49406f15e6ff00ded1babfba9aa54b0bf
xuanhung1509 <vuxuanhung15092002@gmail.com>
2022-11-25 14:39:29 +0700

    chore: add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:react/jsx-runtime', 'prettier'],
+  rules: {
+    'no-shadow': 'warn',
+    'no-unused-vars': 'warn',
+    'no-nested-ternary': 'warn',
+    'arrow-body-style': 'warn',
+    'import/order': 'warn',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'warn',
+    'react/jsx-no-useless-fragment': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
